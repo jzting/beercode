9740bc6de5dc9c5236ea8b1c20bf39379c2ae324
Jason Ting <jzting@gmail.com>
2012-12-19 23:09:42 -0800

    Initial Commit

---
+++ beercode/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  beercode
+//
+//  Created by Jason Ting on 12/19/12.
+//  Copyright (c) 2012 Jason Ting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
